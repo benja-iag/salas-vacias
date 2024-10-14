4c40bd6ce252ae1d35a1a2d450da122e57f0fedb
benja-iag <benjamin.alonso1@mail.udp.cl>
2024-10-14 18:03:25 -0300

    feat: Analytics from vercel and footer added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
-import { Inter } from "next/font/google";
 import "./globals.css";
 import Typography from "@mui/material/Typography";
 import { getSalas } from "./axios/getSalas";
+import { Analytics } from "@vercel/analytics/react";
+import GitHubIcon from "@mui/icons-material/GitHub";
 
-const inter = Inter({ subsets: ["latin"] });
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -12,14 +12,32 @@ export default function RootLayout({
   getSalas();
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
         <Typography
-          sx={{ m: 3, textAlign: "center", fontWeight: "medium", fontSize: 46 }}
-          className="mt 3"
+          sx={{ fontWeight: "medium", fontSize: 46, textAlign: "center" }}
         >
           Salas Vacías FIC
         </Typography>
         {children}
+        <Analytics />
+        <footer>
+          Proyecto de{" "}
+          <a
+            href="https://github.com/open-source-udp"
+            style={{
+              textDecoration: "underline",
+            }}
+          >
+            Open source UDP
+            <GitHubIcon
+              sx={{
+                verticalAlign: "middle",
+                marginRight: "8px",
+                marginLeft: "8px",
+              }}
+            />
+          </a>
+        </footer>
       </body>
     </html>
   );
